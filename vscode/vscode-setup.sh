aaf1a69ea08a2f95634cd04883311e5769c11d98
李適安 <shawn@lishiandeMacBook-Pro.local>
2020-02-03 15:13:53 +0800

    Modify setup

--- vscode/vscode-setup.sh
+++ vscode/vscode-setup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cat vscode.extensions | xargs code --install-extension
+cat vscode.extensions | xargs -n 1 code --install-extension
+cp settings.json ~/Library/Application\ Support/Code/User/
+cp keybindings.json ~/Library/Application\ Support/Code/User/
